17be4d327a4f25ede5de179e3f0e9febec0bd50c
diabloexodia <diabloexodia@gmail.com>
2023-10-11 22:58:03 +0530

    port

--- todoapp.py
+++ todoapp.py
@@ -28,35 +28,5 @@ def delete_task(index):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__)
-
-# Sample list of tasks (you can replace this with a database)
-tasks = []
-
-
-@app.route("/")
-def index():
-    return render_template("index.html", tasks=tasks)
-
-
-@app.route("/add_task", methods=["POST"])
-def add_task():
-    task = request.form.get("task")
-    if task:
-        tasks.append(task)
-    return redirect(url_for("index"))
-
-
-@app.route("/delete_task/<int:index>")
-def delete_task(index):
-    if 0 <= index < len(tasks):
-        del tasks[index]
-    return redirect(url_for("index"))
-
-
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=5000)
